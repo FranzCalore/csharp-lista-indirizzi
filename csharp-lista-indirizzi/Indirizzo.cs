d13c6bd0d0fa200fcf755348942ccde48db56bc4
FranzCalore <franzkaiser1993@gmail.com>
2022-12-12 14:21:58 +0100

    Completato il primo aspetto dell'esercizio, procedo a alcune riflessioni, circa la correttezza delle informazioni inserite

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        private string Name;
+        private string Surname;
+        private string Street;
+        private string City;
+        private string Province;
+        private string Zip;
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.Zip = zip;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public string GetSurname()
+        {
+            return this.Surname;
+        }
+
+        public string GetStreet()
+        {
+            return this.Street;
+        }
+
+        public string GetCity()
+        {
+            return this.City;
+        }
+
+        public string GetProvince()
+        {
+            return this.Province;
+        }
+
+        public string GetZip()
+        {
+            return this.Zip;
+        }
+
+        public void SetName(string Name)
+        {
+            if (this.Name == "undefined")
+            {
+                this.Name = Name;
+            }
+        }
+
+        public void SetSurname(string Surname)
+        {
+            if (this.Surname == "undefined")
+            {
+                this.Surname = Surname;
+            }
+        }
+
+        public void SetStreet(string Street)
+        {
+            if (this.Street == "undefined")
+            {
+                this.Street = Street;
+            }
+        }
+
+        public void SetCity(string City)
+        {
+            if (this.City == "undefined")
+            {
+                this.City = City;
+            }
+        }
+
+        public void SetProvince(string Province)
+        {
+            if (this.Province == "undefined")
+            {
+                this.Province = Province;
+            }
+        }
+
+        public void SetZip(string Zip)
+        {
+            if (this.Zip == "undefined")
+            {
+                this.Zip = Zip;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\n\n----- "
+                   + this.GetName()
+                   + " " 
+                   + this.GetSurname() 
+                   + " -----\n\nStrada: " 
+                   + this.GetStreet() 
+                   + "\nCittà: " 
+                   + this.GetCity() 
+                   + "\nProvincia: " 
+                   + this.GetProvince() 
+                   + "\nZip: " 
+                   + this.GetZip();
+        }
+
+    }
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,13 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using csharp_lista_indirizzi;
+
 string pathFile = "C:\\Users\\Fran_\\Desktop\\Esercizi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\files\\addresses.csv";
 
 StreamReader file = File.OpenText(pathFile);
 
+int rigacorrente=0;
+List<Indirizzo> listaIndirizzi= new List<Indirizzo>();
+
 while (!file.EndOfStream)
 {
+    rigacorrente++;
     string line = file.ReadLine();
-    Console.WriteLine(line);
+    if (rigacorrente != 1)
+    {
+        Console.WriteLine(line);
+        string[] Listainformazioni = line.Split(',');
+        int numeroInformazioni = (Listainformazioni.Length);
+        if (numeroInformazioni < 6)
+        {
+            Console.WriteLine("L'indirizzo a riga " + rigacorrente + " non presenta tutte le informazioni necessarie né la giusta formattazione");
+        }
+        else
+        {
+            for (int i = 0; i < Listainformazioni.Length; i++)
+            {
+                if (Listainformazioni[i]==null || Listainformazioni[i] == "")
+                {
+                    Listainformazioni[i] = "undefined";
+                }
+            }
+            Indirizzo indirizzo = new Indirizzo(Listainformazioni[0], Listainformazioni[1], Listainformazioni[2], Listainformazioni[3], Listainformazioni[4], Listainformazioni[5]);
+            listaIndirizzi.Add(indirizzo);
+        }
+    }
+    
 }
 
 file.Close();
+
+Console.WriteLine(listaIndirizzi[5]);
