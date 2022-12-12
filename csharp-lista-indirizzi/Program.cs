6fdd21216c2070412ceae00b5dede89d4be51f84
FranzCalore <franzkaiser1993@gmail.com>
2022-12-12 13:11:57 +0100

    aperto il file

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,1 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+string pathFile = "C:\\Users\\Fran_\\Desktop\\Esercizi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\files\\addresses.csv";
+
+StreamReader file = File.OpenText(pathFile);
+
+while (!file.EndOfStream)
+{
+    string line = file.ReadLine();
+    Console.WriteLine(line);
+}
+
+file.Close();
